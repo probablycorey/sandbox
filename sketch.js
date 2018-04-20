ecbf38c63154981775a799ef672d9cc1cbd1be8f
Corey Johnson <probablycorey@github.com>
2018-04-20 11:57:47 -0700

    oh cool

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 function setup() {
-  createCanvas(300, 300)
+  createCanvas(300, 300);
 }
 
 function draw() {
-  background('black')
-  fill('red')
-  ellipse(150, 150, 50)
+  background("black");
+  fill("red");
+  ellipse(150, 150, 50);
 }
+
+// this is a comment
